35c0feee81b66507508d0c8b0bac8bdb55c2eea0
Roy Jang <jangroy@hotmail.com>
2017-10-02 15:50:52 -0700

    menu animations

---
+++ Assets/Scripts/menuCubeRot.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuCubeRot : MonoBehaviour {
+
+	public float speed = 10f;  // degrees per second to rotate in each axis. Set in inspector.
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.right, speed * Time.deltaTime);
+	}
+}
+
