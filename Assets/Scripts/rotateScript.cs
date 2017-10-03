bc3c8ef0c7a866bca216d627f518769edc4f32fa
Roy Jang <jangroy@hotmail.com>
2017-10-03 02:51:18 -0700

    testscene

--- Assets/Scripts/rotateScript.cs
+++ Assets/Scripts/rotateScript.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class rotateScript : MonoBehaviour {
 
-	float rotSpeed = 20;
+	float rotSpeed = 80;
 
 	void OnMouseDrag() {
 		float rotX = Input.GetAxis ("Mouse X") * rotSpeed * Mathf.Deg2Rad;
 		float rotY = Input.GetAxis ("Mouse Y") * rotSpeed * Mathf.Deg2Rad;
 
-		transform.RotateAround (Vector3.up, -rotX);
-		transform.RotateAround (Vector3.right, rotY);
+		transform.Rotate (Vector3.up, -rotX);
+		transform.Rotate (Vector3.right, rotY);
 
 	}
 }
