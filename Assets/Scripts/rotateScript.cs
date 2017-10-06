0be2dec4bc4c73c7d1ebe27d12c37bb56490e45c
Byeng hee Kang <byengheekang@gmail.com>
2017-10-05 22:01:42 -0700

    object1 is rotateable.

--- Assets/Scripts/rotateScript.cs
+++ Assets/Scripts/rotateScript.cs
@@ -11,7 +11,7 @@ public class rotateScript : MonoBehaviour {
 		float rotX = Input.GetAxis ("Mouse X") * rotSpeed * Mathf.Deg2Rad;
 		float rotY = Input.GetAxis ("Mouse Y") * rotSpeed * Mathf.Deg2Rad;
 
-		transform.Rotate (Vector3.up, -rotX);
-		transform.Rotate (Vector3.right, rotY);
+		transform.parent.RotateAround (Vector3.up, -rotX);
+		transform.parent.RotateAround (Vector3.right, rotY);
 	}
 }
