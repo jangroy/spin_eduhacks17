37e66a50da4140146fd8a453bb8b42cd867e5c51
jangroy <jangroy@hotmail.com>
2017-10-05 22:04:24 -0700

    fix rotate

--- Assets/Scripts/rotateScript.cs
+++ Assets/Scripts/rotateScript.cs
@@ -7,11 +7,11 @@ public class rotateScript : MonoBehaviour {
 	float rotSpeed = 80;
 
 	void OnMouseDrag() {
-		// Debug.Log ("hey you are touching");
+		Debug.Log ("hey you are touching");
 		float rotX = Input.GetAxis ("Mouse X") * rotSpeed * Mathf.Deg2Rad;
 		float rotY = Input.GetAxis ("Mouse Y") * rotSpeed * Mathf.Deg2Rad;
 
-		transform.Rotate (Vector3.up, -rotX);
-		transform.Rotate (Vector3.right, rotY);
+		transform.parent.RotateAround (Vector3.up, -rotX);
+		transform.parent.RotateAround (Vector3.right, rotY);
 	}
 }
